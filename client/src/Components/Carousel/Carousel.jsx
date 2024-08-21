485a7134f2ed24050aeb34b54630b19ac1bf4e83
peterjbone <peterjbone41@gmail.com>
2024-08-21 09:13:38 -0500

    Bajando velocidad de carrusel y agregando subtitulo

--- client/src/Components/Carousel/Carousel.jsx
+++ client/src/Components/Carousel/Carousel.jsx
@@ -1,47 +1,49 @@
-import "./Carousel.css"
-import { useEffect } from "react"
-import CardOne from "../CardOne/CardOne.jsx"
+import "./Carousel.css";
+import { useEffect } from "react";
+import CardOne from "../CardOne/CardOne.jsx";
 
 function Carousel({ defaultPokemons }) {
 	//* EFECTO CARRUSEL - CÓDIGO JS
 	useEffect(() => {
 		//? Lo pongo en plural por si necesito seleccionar mas scrollers
 		// Array of <div> elements
-		const scrollers = document.querySelectorAll(".scroller")
+		const scrollers = document.querySelectorAll(".scroller");
 
 		//? por si el usuario redujo las animaciones en su navegador
 		if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-			addAnimation()
+			addAnimation();
 		}
 
 		//? funcion que hara la animación
 		function addAnimation() {
 			// Each <div> element
 			scrollers.forEach((scroller) => {
-				scroller.setAttribute("data-animated", true)
+				scroller.setAttribute("data-animated", true);
 
 				// <ul> element
-				const scrollerInner = scroller.querySelector(".scroller__inner")
+				const scrollerInner = scroller.querySelector(".scroller__inner");
 				// Array of <li> elements
-				const scrollerContent = Array.from(scrollerInner.children)
+				const scrollerContent = Array.from(scrollerInner.children);
 
 				scrollerContent.forEach((item) => {
-					const duplicatedItem = item.cloneNode(true)
-					scrollerInner.appendChild(duplicatedItem)
-				})
-			})
+					const duplicatedItem = item.cloneNode(true);
+					scrollerInner.appendChild(duplicatedItem);
+				});
+			});
 		}
-	}, [])
+	}, []);
 
+	//********* COMPONENTE CARRUSEL
 	return (
-		<div className="scroller" data-direction="left" data-speed="fast">
+		<div className="scroller" data-direction="left" data-speed="slow">
+			<h2 className="subtitulo">Algunos pokemones de primera generación:</h2>
 			<ul className="cards-list scroller__inner">
 				{defaultPokemons.map((pokemon) => (
 					<li key={pokemon.id}>{<CardOne pokemon={pokemon} />}</li>
 				))}
 			</ul>
 		</div>
-	)
+	);
 }
 
-export default Carousel
+export default Carousel;
