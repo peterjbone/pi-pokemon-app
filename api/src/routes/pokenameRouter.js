14e5dbd8100c9bc4b4a91f3d0dae4c275385bec2
peterjbone <peterjbone41@gmail.com>
2024-08-31 09:12:52 -0500

    Haciendo anotaciones

--- api/src/routes/pokenameRouter.js
+++ api/src/routes/pokenameRouter.js
@@ -4,6 +4,8 @@ const router = Router();
 const { getPokemonByName } = require("../controllers/pokenameController.js");
 
 //* Para buscar a un pokemon por nombre (futuramente para details)
+//* caso 1: usuario lo busca por barra de busqueda
+//* caso 2: usuario hace click en una card de pokemon
 router.get("/", getPokemonByName); //? FINALIZADO (tiene el query: "name", devuelve le pokemon buscado y SI LO GUARDA EN BD)
 
 module.exports = router;
