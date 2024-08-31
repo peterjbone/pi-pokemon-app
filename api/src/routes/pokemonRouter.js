49372899ecdfe3df1b1ab85e42c68c92a2fb2235
peterjbone <peterjbone41@gmail.com>
2024-08-30 21:10:15 -0500

    pokemonRouter y pokemonController actualizados

--- api/src/routes/pokemonRouter.js
+++ api/src/routes/pokemonRouter.js
@@ -14,6 +14,8 @@ router.get("/", getAllPokemons); //? FINALIZADO (devuelve 40 pokemones, "tipos" 
 router.post("/", createPokemon); //? FINALIZADO (crea un nuevo pokemon, devuelve el pokemon nuevo y SI LO GUARDA EN BD)
 //* Esta ruta se usa para los Details
 router.get("/:id", getPokemonById); //? FINALIZADO (tiene un params, devuelve el pokemon del id que busque y NO lo guarda en BD)
+//* Para buscar a un pokemon por nombre
+router.get("/:nombre");
 router.delete("/:nombre", deletePokemon); //? FINALIZADO (tiene un params, elimina al pokemon de la BD y devuelve todos los pokemones que quedan en la BD)
 
 module.exports = router;
