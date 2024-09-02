6fd89591286f938984a34143bf349ba1a691bd4d
peterjbone <peterjbone41@gmail.com>
2024-09-01 22:57:09 -0500

    cambios en el controlador getAllPokemons

--- api/src/controllers/pokemonController.js
+++ api/src/controllers/pokemonController.js
@@ -3,14 +3,13 @@ const { Pokemon, Type } = require("../db.js");
 
 const getAllPokemons = async (req, res) => {
 	try {
-		const response1 = await axios.get("https://pokeapi.co/api/v2/pokemon");
+		const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
 
-		const packPokemons1 = await Promise.all(
-			response1.data.results.map(async (pokemon) => {
+		const pokemons = await Promise.all(
+			response.data.results.map(async (pokemon) => {
 				const { data } = await axios.get(pokemon.url);
-				const { id, forms, stats, sprites, height, weight, types } = data;
+				const { forms, stats, sprites, height, weight, types } = data;
 				const newPokemon = {
-					id,
 					nombre: forms[0].name,
 					imagen: sprites.other["official-artwork"]["front_default"],
 					vida: stats[0]["base_stat"],
@@ -26,33 +25,7 @@ const getAllPokemons = async (req, res) => {
 			})
 		);
 
-		const response2 = await axios.get(
-			"https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"
-		);
-
-		const packPokemons2 = await Promise.all(
-			response2.data.results.map(async (pokemon) => {
-				const { data } = await axios.get(pokemon.url);
-				const { id, forms, stats, sprites, height, weight, types } = data;
-				const newPokemon = {
-					id,
-					nombre: forms[0].name,
-					imagen: sprites.other["official-artwork"]["front_default"],
-					vida: stats[0]["base_stat"],
-					ataque: stats[1]["base_stat"],
-					defensa: stats[2]["base_stat"],
-					velocidad: stats[5]["base_stat"],
-					altura: height,
-					peso: weight,
-					tipos: types.map((el) => el.type.name)
-				};
-
-				return newPokemon;
-			})
-		);
-
-		const fullPokemons = packPokemons1.concat(packPokemons2);
-		return res.status(200).json(fullPokemons);
+		return res.status(200).json(pokemons);
 	} catch (error) {
 		return res.status(500).send(error.message);
 	}
