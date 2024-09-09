072b98d7ec795a9a59fe1c7fac205e75e95205df
peterjbone <peterjbone41@gmail.com>
2024-09-09 15:41:06 -0500

    Implementando zustand en el frontend

--- client/src/stores/pokemonStore.js
+++ client/src/stores/pokemonStore.js
@@ -1,9 +1,14 @@
 import { create } from "zustand";
+import axios from "axios";
 
 //* store
 export const usePokemonStore = create((set, get) => ({
 	//* initial states
 	allPokemons: [],
-	selectedPokemons: []
+	selectedPokemons: [],
 	//* actions
+	getAllPokemons: async () => {
+		const endpoint = "";
+		const { data } = axios.get(`${endpoint}`);
+	}
 }));
