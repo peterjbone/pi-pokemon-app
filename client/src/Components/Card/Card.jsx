756cb708734d45f76ada7f49924b7cdcddee12a5
peterjbone <peterjbone41@gmail.com>
2024-09-11 17:13:48 -0500

    Cambios del diseño en el frontend

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -2,19 +2,19 @@ import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
 
 function Card({ pokemon }) {
-	const { nombre, ataque, imagen, Types } = pokemon;
+	const { id, nombre, ataque, imagen, Types } = pokemon;
 	return (
 		<div className={styles.card}>
-			<button>X</button>
 			<Link to={`/details/${nombre}`}>
 				<h3>{nombre.toUpperCase()}</h3>
-				<p> Ataque: {ataque} </p>
 				<img src={imagen} alt={nombre} />
-				<h4>Pokemon tipo:</h4>
+				<p>Attack: {ataque} </p>
+				<h4>Types: </h4>
+				{console.log(Types)}
 				<ul>
-					{Types.map((type, index) => {
-						return <li key={index + 1}> {type.nombre} </li>;
-					})}
+					{Types.map((type, index) => (
+						<li key={index + 1}> {type.nombre} </li>
+					))}
 				</ul>
 			</Link>
 		</div>
