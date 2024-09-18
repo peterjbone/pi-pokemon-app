87a055bf6edaf31fa47292d8549cdf978c5ede50
peterjbone <peterjbone41@gmail.com>
2024-09-18 12:03:55 -0500

    Eliminando pagina de FormPokemon del proyecto

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,15 @@
 import styles from "./Navbar.module.css";
-
 import React from "react";
-import { Link } from "react-router-dom";
-import { FaArrowUp } from "react-icons/fa";
-
 import DarkMode from "../DarkMode/DarkMode.jsx";
-import srcPokemonBall from "../../../public/pokebola.png";
 
 const Navbar = () => {
 	//prettier-ignore
+
+	//*********************************** COMPONENT NAVBAR
 	return (
     <div className={styles.navbar}>
-      {/*link to pokemon form*/}
-      <div className={styles.formLink}>
-        <Link to="/form">
-          <img src={srcPokemonBall} alt="pokemon ball" />
-        </Link>
-        <FaArrowUp/>
-        <p>Create your pokemon</p>
-      </div>
       {/*logo*/}
-      <h1>Pokemon Stats</h1>
+      <h1>Poke Data</h1>
       {/*Dark mode*/}
       <DarkMode/>
     </div>
